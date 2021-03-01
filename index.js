ffd50bcfdb2a60c9ba5ae0e8b68bff3100068104
Endzhe Galieva <endzhe91@gmail.com>
2021-03-01 12:19:24 +0300

    added closure modal title

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const modal = $.modal()
+const modal = $.modal({
+  title: 'Endzhe Modal',
+  closable: true,
+  content: `
+   <p>Lorem ipsum dolor sit.</p>
+   <p>Lorem ipsum dolor sit.</p>
+  `,
+  width: '200px'
+})
